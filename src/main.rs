b5ca0556a2da3adece7e6781094c65c7c7cf81a6
Josh Campbell <jlcampbell1991@gmail.com>
2026-04-30 17:32:39 -0500

    ping-pong

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use ::axum::{Router, routing::get};
+
+async fn ping() -> &'static str {
+    "pong"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(ping));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8084").await.unwrap();
+    println!("Listening on http://0.0.0.0:8084");
+    axum::serve(listener, app).await.unwrap();
 }
